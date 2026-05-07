87b77e0710267463d198f4334e799d056d967fa3
arian2004 <arian2004feni@gmail.com>
2026-05-07 21:06:19 +0600

    solution of problem-6

--- solutions.ts
+++ solutions.ts
@@ -39,3 +39,27 @@ function toggleReadStatus(book: Book) {
 }
 
 
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Student extends Person {
+  grade: string;
+
+  constructor(name: string, age: number,grade: string){
+    super(name, age);
+    this.grade = grade;
+  }
+
+  getDetails(): string {
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+  }
+}
+
+
